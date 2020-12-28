237804b8c775a01f4dd2eb1c2ab811fbe826bd67
im-dharte <daniel.harte@immediate.co.uk>
2020-12-28 16:05:32 +0000

    Update tinyman-backend.sh

--- bin/tinyman-backend.sh
+++ bin/tinyman-backend.sh
@@ -7,10 +7,7 @@ read REQ
 REQ_URL=$(echo $REQ | cut -f2 -d' ')
 COMMAND=$(echo ${REQ_URL#?} | cut -f1 -d'?')
 QUERY_STRING=$(echo ${REQ_URL#?} | cut -f2 -d'?')
-declare -A param   
-while IFS='=' read -r -d '&' key value && [[ -n "$key" ]]; do
-    param["$key"]=$value
-done <<<"${QUERY_STRING}&"
+declare -A PARAM; for i in "${array[@]}"; do IFS="=" ; set -- $i; PARAM[$1]=$2; done
 
 case $COMMAND in
   '')
@@ -49,7 +46,7 @@ case $COMMAND in
     echo '"reboot",'
     echo '"poweroff",'
     echo '"sysinfo",'
-    echo '"chromecast_get"'
+    echo '"chromecast"'
     echo ']'
     ;;
   reboot)
@@ -78,13 +75,14 @@ case $COMMAND in
     echo
     sysinfo -ej
     ;;
-  chromecast_get)
+  chromecast)
     echo 'HTTP/1.1 200 OK'
     echo 'Content-Type: application/json'
     echo 'Access-Control-Allow-Origin: *'
     echo 'Connection: close'
     echo
-    echo $PARAM
+    echo ${PARAM["ip"]}
+    echo "${PARAM['ip']}"
     curl "http://${PARAM['ip']}:8008/setup/eureka_info?options=detail"
     ;;
   *)
