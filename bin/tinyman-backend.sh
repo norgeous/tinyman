c06b677f65f88a6ee0c1356850a89313b92a1e14
im-dharte <daniel.harte@immediate.co.uk>
2020-12-28 20:04:35 +0000

    Update tinyman-backend.sh

--- bin/tinyman-backend.sh
+++ bin/tinyman-backend.sh
@@ -77,10 +77,11 @@ case $COMMAND in
     ;;
   chromecast)
     echo 'HTTP/1.1 200 OK'
-    echo 'Content-Type: application/json'
+    echo 'Content-Type: text/plain'
     echo 'Access-Control-Allow-Origin: *'
     echo 'Connection: close'
     echo
+    echo chromecast result:
     [[ "${PARAM['action']}" == "info" ]]    && curl "http://${PARAM['ip']}:8008/setup/eureka_info?options=detail"
     [[ "${PARAM['action']}" == "status" ]]  && chromecast --host ${PARAM['ip']} status
     [[ "${PARAM['action']}" == "mute" ]]    && chromecast --host ${PARAM['ip']} mute
@@ -93,5 +94,3 @@ case $COMMAND in
     echo -n "Unknown command: $COMMAND"
     ;;
 esac
-
-# 192.168.0.243
