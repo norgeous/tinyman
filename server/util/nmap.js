a1335f9d533695898781c403d03dcd69e602473b
im-dharte <daniel.harte@immediate.co.uk>
2021-01-10 15:59:47 +0000

    Update nmap.js

--- server/util/nmap.js
+++ server/util/nmap.js
@@ -2,7 +2,23 @@ const { Subnet } = require('netmap');
  
 const nmapPromise = () => {
   const subnet = new Subnet('192.168.0.0');
-  return subnet.scanForOpenPorts([22, 80, 515, 8080, 8384, 9010, 9100]);
+  return subnet.scanForOpenPorts([
+    22,
+    80,
+    3000,
+    8080,
+    8384,
+    9010,
+    9100,
+  ])
+  .then(results => results.map(host => {
+    return {
+      ...host,
+      ports: host.ports.reduce((acc, port) => {
+        return acc.push(port.open ? port : null);
+      }, []),
+    };
+  }));
 };
 
 
